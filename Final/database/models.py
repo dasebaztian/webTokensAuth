c5c4246aa69e2849b1ac9f553ff98d425222e8b3
Sebastian <danielsebastiansanchezm@gmail.com>
2024-12-25 20:04:48 -0600

    Creación de base de datos

--- Final/database/models.py
+++ Final/database/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    usuario = models.CharField(max_length=20, unique=True)
+    nombre = models.CharField(max_length=50)
+    correo = models.CharField(max_length=100)
+    passwd = models.CharField(max_length=129)
+    pubkey = models.CharField(max_length=100)
+    privkey = models.CharField(max_length=100)
