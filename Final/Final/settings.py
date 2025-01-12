5ee6352533d92bc3e76baabf234f0c40950f84b5
Sebastian <danielsebastiansanchezm@gmail.com>
2025-01-11 21:33:15 -0600

    Revert "Conexion con mysql mas requisitos"
    This reverts commit 761159aa8c058ef50953425f79554ce171803d0b.

--- Final/Final/settings.py
+++ Final/Final/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'Final.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'usuarios',
-        'USER':'isaias',
-        'PASSWORD':'isaias_123',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
