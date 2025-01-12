761159aa8c058ef50953425f79554ce171803d0b
IsaLanCe <landacervantesisaias@gmail.com>
2025-01-11 20:25:44 -0600

    Conexion con mysql mas requisitos

--- Final/Final/settings.py
+++ Final/Final/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Final.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'usuarios',
+        'USER':'isaias',
+        'PASSWORD':'isaias_123',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
